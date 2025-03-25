d448d939f3982c718343eef89dc949d1fbfb12c7
Mylliah <mezhoud.my@gmail.com>
2025-03-25 21:40:18 +0100

    feat: Add main.h with prototypes and format specifier structure

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * struct format - structure for format specifier handling
+ * @type: the format specifier character
+ * @f: pointer to the corresponding handler function
+ */
+typedef struct format
+{
+	char type;
+	int (*f)(va_list);
+} print_format;
+
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_specifier(va_list args);
+void  _putchar(char c);
+int handle_specifiers(const char *format, va_list args, int *index);
+
+#endif /* MAIN_H */
